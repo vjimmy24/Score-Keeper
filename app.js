1ded8ef4fd3adb4ef8659a652d5be70010adce4e
Jimmy Vu <92957140+vjimmy24@users.noreply.github.com>
2022-06-23 16:40:25 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const p2Button = document.querySelector('#p2Button')
 const resetButton = document.querySelector('#reset')
 const p1Text = document.querySelector('#playerOne')
 const p2Text = document.querySelector('#playerTwo')
+const winningSelect = document.querySelector('#score')
+const endText = document.querySelector('#endText')
 
 let p1Score = 0;
 let p2Score = 0;
@@ -19,6 +21,8 @@ p1Button.addEventListener('click', function (e) {
     if (p1Score === winningScore) {
         isGameOver = true; //once this is true, the entire block of code will no longer run.
         //thus, the code cannot be incremented anymore.
+        p1Text.classList.add('has-text-success')
+        p2Text.classList.add('has-text-danger')
     }
     p1Text.textContent = p1Score;
 })
@@ -29,6 +33,8 @@ p2Button.addEventListener('click', function (e) {
     }
     if (p2Score === winningScore) {
         isGameOver = true;
+        p2Text.classList.add('has-text-success')
+        p1Text.classList.add('has-text-danger')
     }
     p2Text.textContent = p2Score;
 })
@@ -39,13 +45,14 @@ function resetScores() {
     p2Score = 0;
     p1Text.textContent = p1Score;
     p2Text.textContent = p2Score;
+    p1Text.classList.remove('has-text-success','has-text-danger')
+    p2Text.classList.remove('has-text-success','has-text-danger')
 }
+resetButton.addEventListener('click', resetScores)
 
-//resetButton.addEventListener('click', resetScores());
-resetButton.addEventListener('click', function() {
-    isGameOver = false;
-    p1Score = 0;
-    p2Score = 0;
-    p1Text.textContent = 0;
-    p2Text.textContent = 0;
+winningSelect.addEventListener('change', function() {
+    winningScore = parseInt(this.value);
+    resetScores();
+    //this refers to the specific object we're listening for (change)
+    //convert this.value to integer -> this returns a string.
 })
